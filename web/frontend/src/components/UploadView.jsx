b0d0f9a63b56b9305f2e77bc1ca053f6c168e401
NoorFatima-code <noorchudhry2121@gmail.com>
2026-08-11 13:02:31 +0500

    merge conflicts

--- web/frontend/src/components/UploadView.jsx
+++ web/frontend/src/components/UploadView.jsx
@@ -46,7 +46,6 @@ function UploadView({ onUploadSuccess }) {
 
     try {
       const response = await fetch(`${API_BASE}/upload`, {
-      const response = await fetch("http://localhost:5000/upload", {
         method: "POST",
         headers: {
           Authorization: `Bearer ${token}`,
